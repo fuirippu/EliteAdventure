79f7078191584a0eaf0d466ee8d0c458359363f1
Phil <fuirippu@gmail.com>
2016-04-26 15:04:56 +0100

    BugFix: Scanner should not beep for cargo/alloy

--- src/swat.c
+++ src/swat.c
@@ -163,7 +163,8 @@ int add_new_ship(int ship_type, int x, int y, int z, struct vector *rotmat, int 
             {
                 sprintf(buf, "+[%s]", obc_ship_name(type));
                 col = GFX_COL_AA_0;
-                playSound = 1;
+				if ((type != SHIP_ALLOY) && (type != SHIP_CARGO))
+					playSound = 1;
             }
             else if ((type == SHIP_THARGOID) || (type == SHIP_THARGLET))
             {
@@ -193,7 +194,6 @@ int add_new_ship(int ship_type, int x, int y, int z, struct vector *rotmat, int 
             return i;
         }
     }
-
     return -1;
 }
 
